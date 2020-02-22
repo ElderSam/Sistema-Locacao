81ee4e50e52693b625140529eb8a01e5b34ab3ff
ElderSam <eldersamuel98@gmail.com>
2020-02-22 12:08:45 -0300

    modificando footer e pagina Produtos

--- views-cache/footer.2245c3a0bd307b29d09350db616a84e9.rtpl.php
+++ views-cache/footer.2245c3a0bd307b29d09350db616a84e9.rtpl.php
@@ -1,5 +1,19 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>    </div>
-    
+<?php if(!class_exists('Rain\Tpl')){exit;}?>            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Elder & Douglas 2020</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div> <!-- end layoutSidenav_content -->
+    </div> <!-- end layoutSidenav -->
+
     <script src="/res/plugins/jquery/jquery-3.4.1.min.js"></script>
     <script src="/res/plugins/bootstrap-bundle/bootstrap.bundle.min.js"></script>
     <script src="/res/js/scripts.js"></script>
@@ -10,7 +24,7 @@
     <script src="/res/plugins/dataTables/js/dataTables.bootstrap4.min.js"></script>
     <script src="/res/assets/demo/datatables-demo.js"></script>
 
-    <script src="/res/js/util.js"></script>     
+    <script src="/res/js/util.js"></script>
 </body>
 
 </html>
