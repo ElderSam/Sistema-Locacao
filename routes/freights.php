5fc1cef0bb8c0c91e0c9f8814e9423f1b5ee5917
ElderSam <eldersamuel98@gmail.com>
2020-10-03 22:19:13 -0300

    Fretes: rotas inserir, atualizar e excluir

--- routes/freights.php
+++ routes/freights.php
@@ -29,6 +29,34 @@ $app->get('/freights/json/:id', function($id) {
     echo $freight->loadFreight((int)$id);
 });
 
+/* rota para criar frete (salva no banco) -----------*/
+$app->post("/rents/create", function(){
+    User::verifyLogin();
+
+	$freight = new FreightController();
+	echo $freight->save();
+});
+
+
+/* rota para atualizar frete --------------------------*/
+$app->post("/freights/:idfreight", function($idfreight){ //update
+	
+    User::verifyLogin();
+
+	$freight = new FreightController();
+	$update = true;
+	echo $freight->save($update);	
+});
+
+/* rota para deletar frete --------------------------*/
+$app->post("/freights/:idfreight/delete", function($idfreight){
+    
+    User::verifyLogin();
+
+    $freight = new FreightController();
+    echo $freight->delete($idfreight);
+});
+
 $app->post('/freights/list_datatables', function(){ //ajax list datatables
 
 	User::verifyLogin();
