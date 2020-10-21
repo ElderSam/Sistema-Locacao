7e92f1ad9818e84a1ab07c9360781bb91da5421e
ElderSam <eldersamuel98@gmail.com>
2020-10-21 18:18:28 -0300

    Faturas: pegando cada contrato e verificando se tem medição

--- vendor/locacao/php-classes/src/Controller/InvoiceController.php
+++ vendor/locacao/php-classes/src/Controller/InvoiceController.php
@@ -4,6 +4,7 @@ namespace Locacao\Controller;
 
 use \Locacao\Generator;
 use \Locacao\Model\Invoice;
+use DateTime;
 
 class InvoiceController extends Generator //controller de Fatura
 {
@@ -19,6 +20,31 @@ class InvoiceController extends Generator //controller de Fatura
         $arrContracts = $this->fatura->getContracts();  // PEGA TODOS OS CONTRATOS QUE POSSUEM ALUGUEIS 
         //print_r($arrContracts);
         $arrContracts = json_decode($arrContracts, true);
+
+        $faturasPendentes = [];
+
+        foreach($arrContracts as $arrContrato)
+        { //para cada contrato
+            $faturasPendentes[] = $this->getAlugueisParaFaturar($arrContrato);
+        }
+
+    }
+
+    function getAlugueisParaFaturar($arrContrato) { //para cada Contrato
+        $contrato = $arrContrato['contrato'];
+        print_r($contrato);
+        echo "<br><br>";
+
+        if($contrato['temMedicao']) // se o contrato tiver regra para faturar
+        {
+
+
+        } else {
+            echo "<br>NÃO TEM MEDIÇÃO<br>";
+            /* PARA A PRIMEIRA FATURA, PEGA O PRIMEIRO ALUGUEL, 
+                A PARTIR DA DATA DE INICIO DESTE CONTA 30 DIAS PARA O VENCIMENTO */   
+        }
+
     }
 
 }
