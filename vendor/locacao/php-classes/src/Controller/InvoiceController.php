5bb1ac5d488188f32224ea67cc5ac0933df9d302
ElderSam <eldersamuel98@gmail.com>
2020-10-23 17:12:32 -0300

    Faturas: método getExactDate a partir do numSemana e diaSemana

--- vendor/locacao/php-classes/src/Controller/InvoiceController.php
+++ vendor/locacao/php-classes/src/Controller/InvoiceController.php
@@ -43,7 +43,7 @@ class InvoiceController extends Generator //controller de Fatura
         $hoje = new DateTime();
         echo "HOJE: ". $hoje->format('Y-m-d');
 
-        //$ultimaFatura = $this->fatura->getultimaFatura(); //BUSCA A ÚLTIMA FATURA DO CONTRATO
+        $ultimaFatura = $this->fatura->getultimaFatura($contrato['idContrato']); //BUSCA A ÚLTIMA FATURA DO CONTRATO
         //DEVE VERIFICAR SE JÁ NÃO TEVE UMA FATURA NESSE MÊS
         //PEGA A DATA DE EMISSÃO DA FATURA
         //$dtUltimaFatura = $ultimaFatura['fatura']['dtEmissao'];
@@ -66,17 +66,20 @@ class InvoiceController extends Generator //controller de Fatura
             {
                 echo "<br>regraFatura: ".$contrato['regraFatura'];
                 $diaVencFatura = $contrato['diaFatura'];
+
                 if($contrato['regraFatura'] == 1) // se tiver dia fixo, verificar se é o dia de hoje (dia atual) 
                 {        
                     //NÃO FAZ NADA
 
-                }else if($contrato['regraFatura'] == 2)
-                { //se a regra para faturar for dia da semana
+                }else if($contrato['regraFatura'] == 2) //se a regra para faturar for dia da semana
+                {
                     $diaVencFatura = $this->getExactDate($contrato['semanaDoMes'], $diaVencFatura, $hoje);
                     $diaVencFatura = $diaVencFatura->format('Y-m-d');
+                    echo "<br>diaVencFatura calculado: $diaVencFatura";
                 }
 
             }
+
 
         } else {
             echo "<br>NÃO TEM MEDIÇÃO<br>";
@@ -84,6 +87,40 @@ class InvoiceController extends Generator //controller de Fatura
                 A PARTIR DA DATA DE INICIO DESTE CONTA 30 DIAS PARA O VENCIMENTO */   
         }
 
+    }
+
+    function getExactDate($numSemana, $diaSemana, $hoje) // retorna a data Y-m-d a partir do número da semana dentro do mês e do dia dentro da semana
+    {
+        //PERCORRER O MÊS, ATÉ CHEGAR NA SEMANA DO CONTRATO
+         //PERCORRER A SEMANA ATÉ CHEGAR NO DIA DA SEMANA -> ENCONTRAR A DATA EXATA
+
+        $auxDate = new DateTime($hoje->format('Y-m') . '-01'); //primeiro dia do mês
+        $auxDayInWeek = $auxDate->format('N') + 1;
+        $auxNumWeek = 0;
+
+        if($diaSemana >= $auxDayInWeek) { //se o dia da semana desejado for maior que o primeiro dia do mês
+            $auxNumWeek += 1;
+            echo "dia da semana do contrato é maior que o dia 01 do mês";
+        }
+
+        $auxNumWeek += 1;
+        $auxDif = abs($auxDayInWeek - $diaSemana); //retorna o valor absoluto da diferença
+        /*echo "<br><br>auxDayInWeek: $auxDayInWeek <br>
+            diaSemana do Contrato: $diaSemana, diferença: $auxDif <br> numSemana do Contrato: $numSemana";*/
+
+        $auxDate = $auxDate->add(new DateInterval('P0'.(7 - $auxDif).'D'));
+
+        //echo "<br>date: ". $auxDate->format('Y-m-d') . " auxNumWeek: " .$auxNumWeek;
+
+        //$auxDate = $auxDate->add(new DateInterval('P07D'));
+
+        while($auxNumWeek < $numSemana)
+        {
+            $auxDate = $auxDate->add(new DateInterval('P07D'));
+            $auxNumWeek += 1;
+        }
+
+        return $auxDate;
     }
 
 }
