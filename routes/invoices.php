a2bf1c3897cf2dbcb9588c3f4e9b1c53c9ba689a
ElderSam <eldersamuel98@gmail.com>
2020-10-25 17:14:13 -0300

    Faturas: criando rota para a página de faturas

--- routes/invoices.php
+++ routes/invoices.php
@@ -5,11 +5,22 @@ use \Locacao\Model\User;
 use \Locacao\Controller\InvoiceController;
 use \Locacao\Model\Invoice;
 
-/* FATURA
-    getFaturasParaFazer()
+/* FATURA */
+function getPageInvoice($idInvoice=false) {
+    User::verifyLogin();
 
- */
+    $page = new Page();
+    $page->setTpl("faturas", array(
+        "idInvoiceURL"=>$idInvoice
+    ));
+}
+
 /* rota para página de faturas --------------*/
+$app->get('/invoices', function(){
+    getPageInvoice();
+});
+
+/* rota para pegar lista de faturas pendentes (para fazer) */
 $app->get('/invoices/json/pending', function(){
     //User::verifyLogin();
     //echo "rota faturas<br>";
