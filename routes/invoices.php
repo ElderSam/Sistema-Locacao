7f1f17f75fff9114403474431551aec137f8c528
Numeriano <douglas_rnmeriano@hotmail.com>
2020-10-31 05:15:37 -0300

    Criação do arquivo invoices.js e alterações feitas na fatura_salvar.html

--- routes/invoices.php
+++ routes/invoices.php
@@ -5,15 +5,46 @@ use \Locacao\Model\User;
 use \Locacao\Controller\InvoiceController;
 use \Locacao\Model\Invoice;
 
-/* FATURA
-    getFaturasParaFazer()
 
- */
 /* rota para página de faturas --------------*/
 $app->get('/invoices', function(){
-    //User::verifyLogin();
-    echo "rota faturas";
-    $faturaController = new InvoiceController();
-    echo $faturaController->getFaturasParaFazer();
+
+    User::verifyLogin();
+	
+	$page = new Page();
+	$page->setTpl("faturas");
 });
+
+/* rota para página de fatura_salvar --------------*/
+$app->get('/invoices/new', function(){
+
+    User::verifyLogin();
+	
+	$page = new Page();
+	$page->setTpl("fatura_salvar", [
+		"idFatura"=>'0'
+	]);
+  
+});
+
+$app->post('/invoices/list_datatables', function(){ //ajax list datatables
+
+	User::verifyLogin();
+	
+	//Receber a requisão da pesquisa 
+	$requestData = $_REQUEST;
+
+	$invoices = new InvoiceController();
+	echo $invoices->ajax_list_contracts($requestData);
+	
+});
+
+
+// /* rota para página de faturas --------------*/
+// $app->get('/invoices', function(){
+//     //User::verifyLogin();
+//     // echo "rota faturas";
+//     $faturaController = new InvoiceController();
+//     echo $faturaController->getFaturasParaFazer();
+// });
 
